5c745ab52cc7af6c7035a85e94b1aca480b968f6
ekromvpn <asusring1@gmail.com>
2024-08-05 03:12:34 +0700

    Update setup.sh

--- badvpn/setup.sh
+++ badvpn/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/NevermoreSSH/VVV/main/"
+REPO="https://raw.githubusercontent.com/ekromvpn/SSH/main/"
 wget -O /usr/sbin/badvpn "${REPO}badvpn/badvpn" >/dev/null 2>&1
 chmod +x /usr/sbin/badvpn > /dev/null 2>&1
 wget -q -O /etc/systemd/system/badvpn1.service "${REPO}badvpn/badvpn1.service" >/dev/null 2>&1
